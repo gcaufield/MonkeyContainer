35617a8fd9bce4bc097429f2377533ab14a064d1
Greg Caufield <greg@embeddedcoffee.ca>
2020-10-01 21:06:38 -0600

    Correct issue with build command in build.sh
    Forgot the '.' as I frequently do on the cli

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 VERSION="v0.0.0"
 
-docker build --no-cache -t "gcaufield/monkeycontainer:$VERSION" --label vcs_rev=`git describe --long` --label created=`date +"%Y-%m-%d"`
+docker build --no-cache -t "gcaufield/monkeycontainer:$VERSION" --label vcs_rev=`git describe --long` --label created=`date +"%Y-%m-%d"` .
