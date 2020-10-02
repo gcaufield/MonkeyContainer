cdc5fce8f4dc766060600ec74bcecc6976af2690
Greg Caufield <greg@embeddedcoffee.ca>
2020-10-02 06:05:01 -0600

    Bump version: 0.1.0 to 0.1.1

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-VERSION="0.1.0"
+VERSION="0.1.1"
 
 docker build --no-cache -t "gcaufield/monkeycontainer:v$VERSION" --build-arg vcs_rev=`git describe --long` --build-arg created=`date +"%Y-%m-%d"` --build-arg version=$VERSION .
